960f256d8adaa4300887e96777840ec4d6dc6444
clam <coderzww@gmail.com>
2018-03-29 00:09:10 +0800

    使用了父子路由的方式定义了reader.create路由

--- books/An in-depth analytis and best practices for angularjs/BookForum/app/configs/router.js
+++ books/An in-depth analytis and best practices for angularjs/BookForum/app/configs/router.js
@@ -1,23 +1,40 @@
-'use strict';
+"use strict";
 
-angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('default', {
-    url: '',
-    templateUrl: 'controllers/home/index.html',
-    controller: 'HomeIndexCtrl'
-  });
+// 使用模块的config方法: 回调函数在模块加载时运行, 以便对模块进行配置, 路由就是配置的一种
+angular.module("app").config(function($stateProvider, $urlRouterProvider) {
+    // 声明一个state
+    $stateProvider.state("default", {
+        url: "",
+        templateUrl: "controllers/home/index.html",
+        controller: "HomeIndexCtrl as vm"
+    });
 
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl: 'controllers/home/index.html',
-    controller: 'HomeIndexCtrl'
-  });
+    $stateProvider.state("home", {
+        url: "/",
+        templateUrl: "controllers/home/index.html",
+        controller: "HomeIndexCtrl as vm"
+    });
 
-  $stateProvider.state('notFound', {
-    url: '/notFound',
-    templateUrl: 'controllers/home/notFound.html',
-    controller: 'HomeNotFoundCtrl'
-  });
+    $stateProvider.state("notFound", {
+        url: "/notFound",
+        templateUrl: "controllers/home/notFound.html",
+        controller: "HomeNotFoundCtrl as vm"
+    });
 
-  $urlRouterProvider.otherwise('/notFound');
+    // 定义默认路由
+    $urlRouterProvider.otherwise("/notFound");
+
+    // 定义一个父路由, 只用于提供url
+    $stateProvider.state("reader", {
+        url: "/reader", // 所有的子路由都会继承这个路由
+        template: "<div ui-view></div>",
+        abstract: true // 抽象路由, 不能通过url直接访问
+    });
+
+    // 根据路由名按照 . 分隔, 前面属于父路由
+    $stateProvider.state("reader.create", {
+        url: "/create", // 会和父路由的url拼接上, 完整的是/reader/create
+        templateUrl: "controllers/reader/create.html",
+        controller: "ReaderCreateCtrl as vm"
+    });
 });
