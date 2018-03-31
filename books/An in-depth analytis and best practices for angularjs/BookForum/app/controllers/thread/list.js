5889aa4a828e7e51a9f3d7f406e9c7340a0d3742
clam <coderzww@gmail.com>
2018-03-31 22:55:04 +0800

    主题列表

---
+++ books/An in-depth analytis and best practices for angularjs/BookForum/app/controllers/thread/list.js
@@ -0,0 +1,23 @@
+angular.module("app").controller("ThreadListCtrl", function() {
+    var vm = this;
+    vm.items = [
+        {
+            title: "这是第一个主贴",
+            poster: "clam",
+            dateCreated: "2018-03-31T00:00:00"
+        },
+        {
+            title: "这是第二个主贴",
+            poster: "elsa",
+            dateCreated: "2018-03-31T00:00:00"
+        }
+    ];
+
+    for (let i = 0; i < 10; i++) {
+        vm.items.push({
+            title: "这是第" + (i + 2) + "个主贴",
+            poster: "clam",
+            dateCreated: "2018-03-31T00:00:00"
+        });
+    }
+});
