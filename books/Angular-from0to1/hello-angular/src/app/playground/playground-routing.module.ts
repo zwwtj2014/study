4f93df50926144ea71bccb6ea17db364e8751c34
clam <coderzww@gmail.com>
2018-06-03 00:44:29 +0800

    通过vscode调试angular

--- books/Angular-from0to1/hello-angular/src/app/playground/playground-routing.module.ts
+++ books/Angular-from0to1/hello-angular/src/app/playground/playground-routing.module.ts
@@ -1,11 +1,30 @@
 import { Routes, RouterModule } from '@angular/router';
 import { PlaygroundComponent } from './playground.component';
 import { NgModule } from '@angular/core';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PlaygroundComponent
+    component: PlaygroundComponent,
+    children: [
+      {
+        path: 'one',
+        component: OneComponent
+      },
+      {
+        path: 'two',
+        component: TwoComponent,
+        children: [
+          {
+            path: 'three',
+            component: ThreeComponent
+          }
+        ]
+      }
+    ]
   }
 ];
 
