473967da39cf2fe766de10d507e5a6b8f78bde24
clam <coderzww@gmail.com>
2018-06-06 23:58:42 +0800

    stat

--- books/Node实战/ch4/static-server.js
+++ books/Node实战/ch4/static-server.js
@@ -8,8 +8,24 @@ const root = __dirname;
 const server = http.createServer(function (req, res) {
     const url = parse(req.url);
     const path = join(root, url.pathname);
-    const readStream = fs.createReadStream(path); // 创建一个从磁盘读取文件的流
-    readStream.pipe(res);
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            if ('ENOENT' === err.code) {
+                res.statusCode = 404;
+                res.end('Not Found');
+            } else {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            }
+        }
+        res.setHeader('Content-Length', stats.size);
+        const readStream = fs.createReadStream(path); // 创建一个从磁盘读取文件的流
+        readStream.pipe(res);
+        readStream.on('error', function (err) {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+        });
+    });
 });
 
 server.listen(3000);
