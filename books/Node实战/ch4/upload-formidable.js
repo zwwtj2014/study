dbddf46797da700853cee0df2ff2e342694b3d47
clam <coderzww@gmail.com>
2018-06-08 00:05:50 +0800

    formidable

---
+++ books/Node实战/ch4/upload-formidable.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const formidable = require('formidable');
+
+const server = http.createServer((req, res) => {
+    switch (req.method) {
+        case 'GET':
+            show(req, res);
+            break;
+        case 'POST':
+            upload(req, res);
+            break;
+        default:
+            break;
+    }
+});
+
+function show(req, res) {
+    var html = `
+    <form method="post" action="/" enctype="multipart/form-data">
+        <p><input type="text" name="name"></p>
+        <p><input type="file" name="file"></p>
+        <p><input type="submit" value="Upload"></p>
+    </form>`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(html));
+    res.end(html);
+}
+
+function upload(res, res) {
+    if (!isFormData(req)) {
+        res.statusCode = 400;
+        res.end('Bad Request; expecting multipart/form-data');
+        return;
+    }
+
+    var form = new formidable.IncomingForm();
+
+    // 收到输入域
+    form.on('field', function (field, value) {
+        console.log(field);
+        console.log(value);
+    });
+
+    // 收到文件并处理
+    form.on('file', function (name, file) {
+        console.log(name);
+        console.log(file);
+    });
+
+    form.on('end', function () {
+        res.end('update complete');
+    });
+
+    form.on('progress', function (bytesReceived, byteExpected) {
+        var percent = Math.floor(bytesReceived / byteExpected * 100);
+        console.log(percent);
+    })
+
+    form.parse(req);
+
+}
+
+function isFormData(req) {
+    var type = req.headers['content-type'] || '';
+    return 0 === type.indexOf('multipart/form-data')
+}
