6f34d11700de4a36f111e2891ec87de7d90edc49
clam <coderzww@gmail.com>
2018-07-31 00:49:17 +0800

    rollup 和 npm集成，需要使用rollup-plugin-node-resolve 插件去解析node模块

--- rollup-demo/first-rollup/dist/bundle.js
+++ rollup-demo/first-rollup/dist/bundle.js
@@ -3,9 +3,12 @@
 // src/foo.js
 var foo = 'hello world!';
 
+var index = 42;
+
 // main.js
 function main () {
   console.log(foo);
+  console.log('the answer is ' + index);
 }
 
 module.exports = main;
--- rollup-demo/first-rollup/src/main.js
+++ rollup-demo/first-rollup/src/main.js
@@ -1,5 +1,7 @@
 // main.js
 import foo from './foo.js';
+import answer from 'the-answer';
 export default function () {
   console.log(foo);
+  console.log('the answer is ' + answer);
 }
